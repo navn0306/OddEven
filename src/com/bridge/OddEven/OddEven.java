0a7872eb80ccc54aea969ba09381ab059617bfc1
Navneet <navn0306@gmail.com>
2021-11-28 01:25:16 +0530

    ADD: Added program to check if the given number is ODD or EVEN

--- src/com/bridge/OddEven/OddEven.java
+++ src/com/bridge/OddEven/OddEven.java
@@ -1,7 +1,19 @@
 package com.bridge.OddEven;
 
+import java.util.Scanner;
+
 public class OddEven {
+
     public static void main(String[] args) {
-        System.out.println("\n Welcome, This program checks if the given number is odd or even.");
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
     }
 }
