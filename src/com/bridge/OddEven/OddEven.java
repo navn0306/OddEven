20030c43c44bd75b508ccddd37f3887c3d3e4e4e
Navneet <navn0306@gmail.com>
2021-11-28 01:22:57 +0530

    ADD: Added welcome message for program

---
+++ src/com/bridge/OddEven/OddEven.java
@@ -0,0 +1,7 @@
+package com.bridge.OddEven;
+
+public class OddEven {
+    public static void main(String[] args) {
+        System.out.println("\n Welcome, This program checks if the given number is odd or even.");
+    }
+}
